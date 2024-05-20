614cce2dd7711afcb27f911eb9c5b060ce0d8486
MEGUMMY1 <cys10902@nate.com>
2024-05-20 17:09:04 +0900

    Refactor: Next.js 마이그레이션

---
+++ src/pages/folder.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Main from "../Components/FolderMain/FolderMain";
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
+
+function Folder() {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    );
+}
+
+export default Folder;
---
+++ src/pages/shared.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Main from "../Components/SharedMain/SharedMain";
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
+
+function Shared() {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    );
+}
+
+export default Shared;
