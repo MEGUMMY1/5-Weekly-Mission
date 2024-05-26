389ee5f7cd43dbb0de4cba7f3aeee9dac62ba52b
MEGUMMY1 <cys10902@nate.com>
2024-05-26 23:11:57 +0900

    Feat: Folder Page accessToken 없는 경우 페이지 이동

--- src/pages/folder.tsx
+++ src/pages/folder.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 import Main from "../Components/FolderMain/FolderMain";
 import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer/Footer";
 
 function Folder() {
+    const router = useRouter();
+
+    useEffect(() => {
+        const accessToken = localStorage.getItem("accessToken");
+        if (!accessToken) {
+            router.push("/signin");
+        }
+    }, []);
+
     return (
         <>
             <Header />
